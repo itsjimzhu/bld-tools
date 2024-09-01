e1802c26ea46a01792fd163bea7738a40a2bbdf8
Jim <jimzhu0602@gmail.com>
2024-09-01 13:24:01 +1000

    Remove annoying logging

--- bld_tools.py
+++ bld_tools.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 
 # sys.argv[0] is the script name
-# sys.argv[1] is the first argument, sys.argv[2] is the second, and so on.
+# sys.argv[1] is the first argument, sys.argv[2] is the second, and so on
 if len(sys.argv) == 3:
     file_path = sys.argv[1]
     sheet_name = sys.argv[2]
@@ -13,7 +13,6 @@ if len(sys.argv) == 3:
 else:
     print("Pass args {file_path} {sheet_name}")
     sys.exit(0)
-
 
 sep = ""
 sheet = pd.read_excel(file_path, sheet_name, dtype=str, header=None, index_col=None)
@@ -39,7 +38,6 @@ deck = ga.Deck(
     sheet_name,
 )
 
-print(sheet)
 rows, cols = sheet.shape 
 for i in range(1, rows):
     for j in range(1, cols):
